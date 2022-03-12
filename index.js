e16a129a24d8ed742520ab8df5518748872f8dd5
koressel <koressel.dev@gmail.com>
2022-03-11 21:36:03 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+#!/usr/bin/env node
+
+const execSync = require('child_process').execSync;
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const program = require('commander');
+const moment = require('moment');
+const term = require('terminal-kit').terminal;
+
+const p = require('./package.json'),
+	dayInWeek = 7, chars = ['░', '▒', '▓', '█'],
+	mapRange = (x, inMin, inMax, outMin, outMax) => (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin,
+	readme = `[![](${p.badge})](${p.homepage})  \n[![](${p.logo})](${p.homepage})`;
+let alphabet = require('./alphabet');
+
+program
+	.version(p.version)
+	.option('-s, --startdate [date]', 'Set the start date (rounded to week)')
+	.option('-o, --origin [url]', 'Add origin url')
+	.option('-p, --push', 'Push to origin')
+	.option('-f, --force', 'Force push')
+	.option('-f, --file [absolute path]', 'Specify a pattern file')
+	.option('-t, --text [text]', 'Text to draw')
+	.option('--font [font]', 'Set the font')
+	.option('-i, --invert', 'Invert the colors')
+	.option('-m, --multiplier [n]', 'Multiply the number of commits by n', s => parseInt(s), 1)
+	.option('--flipvertical', 'Flip vertically the spray')
+	.option('--fliphorizontal', 'Flip horizontally the spray')
+	.parse(process.argv);
+
+if (process.argv.length < 3) {
+	program.help();
+}
+
+if (program.push && !program.origin) {
+	console.warn('Option --origin required');
+}
+
+if (program.font) {
+	const fonts = fs.readdirSync(path.resolve(__dirname, 'fonts')).map(f => f.slice(0, -3));
+	if (!fonts.includes(program.font)) {
+		return console.warn(`'${program.font}' font not found!\nFonts available: ${fonts.join(' ')}`);
+	}
+	alphabet = require(`./fonts/${program.font}`);
+}
+alphabet[' '] = alphabet[' '] || new Array(dayInWeek).fill(' ');
+
+let startDate, pattern;
+
+if (program.startdate) {
+	startDate = moment(program.startdate);
+	startDate.day(0);
+} else {
+	startDate = moment.utc();
+	startDate.subtract(53, 'week');
+	startDate.day(7);
+}
+startDate.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+
+if (program.file) {
+	pattern = JSON.parse(fs.readFileSync(program.file, 'utf8'));
+} else if (program.text) {
+	pattern = new Array(dayInWeek).fill(' ');
+	for (const l of program.text) {
+		if (!(l in alphabet)) {
+			console.warn(`'${l}' character not supported`);
+			return console.info('Charset: ' + Object.keys(alphabet).join(' '));
+		}
+		const letter = alphabet[l];
+		for (let i = 0; i < letter.length; i++) {
+			pattern[i] += letter[i] + ' ';
+		}
+	}
+} else {
+	return console.warn('Option --text or --file required');
+}
+
+pattern = pattern.map(l => l.replace(/ /g, 0));
+let matrix = pattern.map(line => line.split('').map(c => parseInt(c)));
+
+let seconds = startDate.unix();
+const folder = 'spray-' + crypto.randomBytes(6).toString('hex'), file = 'readme.md';
+fs.mkdirSync(folder);
+execSync(`git init ${folder}`);
+fs.writeFileSync(`./${folder}/${file}`, readme);
+execSync(`git -C ${folder} add ${file}`);
+term.windowTitle(p.name);
+term.reset();
+term.hideCursor();
+
+const maxWeeks = Math.max(...matrix.map(line => line.length)), area = maxWeeks * dayInWeek;
+
+if (program.invert) {
+	const max = Math.max(...matrix.flat());
+	matrix = matrix.map(line => line.map(c => max - c));
+}
+
+if (program.fliphorizontal) matrix = matrix.map(line => line.reverse());
+if (program.flipvertical) matrix = matrix.reverse();
+
+let commits = 0;
+for (let week = 0; week < maxWeeks; week++) {
+	for (let day = 0; day < dayInWeek; day++) {
+		const dayPassed = week * dayInWeek + day + 1;
+		const progress = dayPassed / area;
+		term.moveTo(1, dayInWeek + 1);
+		term.bar(progress, { barStyle: term.brightWhite, innerSize: maxWeeks });
+		const commitsPerDay = matrix[day][week] * program.multiplier;
+		for (let commit = 0; commit < commitsPerDay; commit++) {
+			let progress2 = mapRange(commit, 0, commitsPerDay - 1, 0, chars.length - 1);
+			if (isNaN(progress2)) progress2 = chars.length - 1;
+			term.moveTo(week + 1, day + 1, chars[progress2]);
+			execSync(`git -C ${folder} commit --allow-empty --date="${seconds}" -am '${p.name}'`);
+			commits++;
+		}
+		seconds += 24 * 60 * 60;
+	}
+}
+term.moveTo(1, dayInWeek + 1);
+term.eraseLine();
+term.hideCursor(0);
+
+console.info(`${folder} generated (${commits} commits), starting date ${startDate.format('ddd MMM DD YYYY')}`);
+
+if (program.origin) {
+	console.info(`Adding origin ${program.origin}`);
+	execSync(`git -C ${folder} remote add origin ${program.origin}`);
+}
+
+if (program.push) {
+	process.stdout.write('Pushing ');
+	execSync(`git -C ${folder} push ${program.force ? '--force' : ''} -u origin master`);
+}
---
+++ readme.md
@@ -0,0 +1,120 @@
+# GitHub Spray
+[![npm](https://img.shields.io/npm/v/github-spray.svg?style=flat-square)](https://www.npmjs.com/package/github-spray)
+[![license](https://img.shields.io/github/license/annihil/github-spray.svg?style=flat-square)]()
+
+<p align="center">
+	<img src="https://i.imgur.com/nPZyGNo.gif" height="200" width="200" alt="GitHub Spray logo"/>
+</p>
+
+A CLI to generate sprays for your GitHub contribution history graph
+
+![](https://i.imgur.com/Of8MjPj.gif)
+
+## Installation
+
+```sh
+npm i -g github-spray
+```
+
+## Requirements
+
+- Git
+- Recent NodeJS
+
+## Usage
+
+1. Create a new GitHub repository and copy its url
+2. 
+```sh
+github-spray -t <text> --multiplier <factor> --push --origin <github_repository_url>
+```
+(example)
+```sh
+github-spray -t hello --multiplier 10 --push --origin https://github.com/John/hello.git
+```
+_Depending on your number of contributions on the given period that you want to spray, you will need to multiply the number of commit per day by a specific factor (--multiplier) so that the spray become more relevant on your calendar heatmap_.  
+
+```sh
+github-spray --help
+```
+
+### Fonts
+```sh
+github-spray -t <text> --font <font_name>
+```
+(example)
+```sh
+github-spray -t Wald0 --font portable_vengeance
+```
+![](https://i.imgur.com/iF2xwwU.png)  
+```sh
+github-spray -t Mario? --font mario
+```
+![](https://i.imgur.com/0P8Dmrn.png)  
+#### led7
+![](https://i.imgur.com/O5EW9IV.png)  
+![](https://i.imgur.com/yUaeoGL.png)  
+![](https://i.imgur.com/mvtC6sE.png)  
+![](https://i.imgur.com/A76n04M.gif)  
+*Fonts available are in the fonts folder*
+
+### Invert colors
+![](https://i.imgur.com/rgSownU.png)
+```sh
+github-spray -t hello -i
+```
+
+### Flip vertical
+![](https://i.imgur.com/xHNnAGb.png)
+```sh
+github-spray -t hello --flipvertical
+```
+
+### Flip horizontal
+![](https://i.imgur.com/LMtZGAr.png)
+```sh
+github-spray -t hello --fliphorizontal
+```
+
+### Custom pattern
+
+To use a custom pattern, create a JSON file like follow.  
+*The numbers (1 to 4) will determine the number of commit per day, and thus the green's darkness.*
+```json
+[
+    "  333  ",
+    " 3   3 ",
+    "3 2 2 3",
+    "3     3",
+    "3 222 3",
+    " 3   3 ",
+    "  333  "
+]
+```
+![](https://i.imgur.com/sZDSnFH.png)
+```sh
+github-spray -f <path/to/pattern.json> ...
+```
+
+![](https://i.imgur.com/QDxcot7.png)
+```sh
+github-spray -f patterns/space-invaders.json ...
+```
+
+**You can use [GitHub Spray Generator](https://annihil.github.io/github-spray-generator/) to graphically draw patterns**
+
+### Custom start date
+```sh
+github-spray --startdate YYYY-MM-DD ...
+```
+*The date will be rounded to the nearest sunday.*  
+By default the start date is the nearest sunday of the current date - 53 weeks.
+
+### Multiplier: scale colors
+```sh
+github-spray -m <factor> ...
+```
+Multiply the number of commit per day by the given factor to make the spray darker or brighter on your calendar heatmap. 
+
+## Known issues
+- Before Monday 10 March 2014 there could be a [shift due to the timezone](https://github.blog/2014-03-07-timezone-aware-contribution-graphs/)
